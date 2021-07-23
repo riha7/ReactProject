7cab8874c2be30d1b83e8bc23b365e9ad26c09ee
riha7 <harik0725@gmail.com>
2021-07-23 18:03:27 +0530

    sample.js modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Sample from './Sample';
 
 function App() {
   return (
     <div className="App">
         <h1>welcome to react</h1>
         <h1>trying to develope and deployee into git</h1>
+          <Sample/>
          </div>
   );
 }
